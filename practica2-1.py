387f455a350f41370706ce5e79a4b397c04a2f9d
xiiomara19 <xiomaracaceres19.07@gmail.com>
2025-04-28 21:29:15 +0200

    Practicas restantes añadidas (SIN FINALIZAR)

---
+++ practica2-1.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+""""PRÁCTICA 2.1. Programar el cálculo de la representación de Casteljau y computar el gráfico de la visualización geométrica."""
+"""XIOMARA GERALDINE CÁCERES CHANCAHUANA"""
+"""LUNES 28/04/2025"""
+
+def mainProgram():
+    return False
+
+
+if __name__ == '__main__':
+    while True:
+        mainProgram()
